0e96a42ec8141ce8254136c3210ea87339eb874b
Tom Huynh <tomhuynhsg@gmail.com>
2022-07-12 23:01:21 +0700

    Initial Commit

---
+++ I Am Rich/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I Am Rich
+//
+//  Created by Tom Huynh on 7/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
