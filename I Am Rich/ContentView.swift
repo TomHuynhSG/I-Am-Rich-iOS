21c81baa129784a1ab8c15bb8e41b4e433507cb9
Tom Huynh <tomhuynhsg@gmail.com>
2022-07-12 23:26:22 +0700

    Add the image views for RMIT logo and red ruby images in the VStack along with the text "I Am Rich"

--- I Am Rich/ContentView.swift
+++ I Am Rich/ContentView.swift
@@ -10,13 +10,25 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-        Color(red: 0.03, green: 0.03, blue: 0.35)
-            .edgesIgnoringSafeArea(.all)
-            
-        Text("I Am Rich")
-                            .font(.system(size: 40))
-                            .fontWeight(.bold)
-                            .foregroundColor(.white)
+            Color(red: 0.03, green: 0.03, blue: 0.35)
+                .edgesIgnoringSafeArea(.all)
+                
+            VStack {
+                Image("rmit-logo-white")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(50.0)
+                    .frame(width: 350.0, alignment: .center)
+                Text("I Am Rich")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                Image("red-diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, alignment: .center)
+            }
+                
         }
     }
 }
