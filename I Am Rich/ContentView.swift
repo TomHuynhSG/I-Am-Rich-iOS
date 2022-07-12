4529955542845c63611c4ea535e445e5331fb3d8
Tom Huynh <tomhuynhsg@gmail.com>
2022-07-12 23:24:49 +0700

    Add ZStack to have the background to be underneath the text view. Also add modifiers to the text view to change the look of the text "I Am Rich"

--- I Am Rich/ContentView.swift
+++ I Am Rich/ContentView.swift
@@ -9,8 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        ZStack {
         Color(red: 0.03, green: 0.03, blue: 0.35)
             .edgesIgnoringSafeArea(.all)
+            
+        Text("I Am Rich")
+                            .font(.system(size: 40))
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+        }
     }
 }
 
